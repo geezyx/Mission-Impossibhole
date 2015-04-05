9b17a4f67e46ee265cd545f046dd005c22a22692
Mike Gee <geezyx@gmail.com>
2015-04-05 12:59:08 -0700

    More recent iteration of code (from 2013) but not latest

--- mission_impossiblhole.c
+++ mission_impossiblhole.c
@@ -2,18 +2,19 @@
 // 2.13.2013
 // Mike Gee
 
+#include <EEPROM.h>
+
 int sensorLaser[8] = {};         //{sensor pin, laser pin, threshold, triggered millis}
 int laserPins[8] = {             // pins with lasers attached
   6, 7, 8, 9, 10, 11, 12, 13 };  // O3, D7, D8, O2, O1, O0, D12, D13 ports on tinkerkit
 int sensorPins[8] = {            // pins with sensors attached
   0, 1, 2, 3, 4, 5, 6, 7 };      // I0, I1, I2, I3, I4, I5, I6, I7 ports on tinkerkit
 int alarmPin = 2;
-int threshold[8] = {
-  15, 15, 15, 15, 15, 15, 15, 15 };
-long timeTriggered[8] = {
+int threshold[8] = { };          // Threshold values, loaded from EEPROM on setup
+long timeTriggered[8] = {        // msec timestamp of each sensor's last triggered time
   0, 0, 0, 0, 0, 0, 0, 0 };
-int numPins = 1;                 // pin count used for testing
-int buttonPin = 31;               // D2 IO port on tinkerkit used for calibration
+int numPins = 8;                 // pin count used for testing
+int buttonPin = 2;              // D2 IO port on tinkerkit used for calibration
 long mainAlarm = 0;
 long time = 0;
 long alarmTime = 0;
@@ -26,8 +27,8 @@ void setup()
   digitalWrite( buttonPin, HIGH );
   digitalWrite( alarmPin, LOW );
   for ( int i=0; i < numPins; i++ ) {
-    
     pinMode( laserPins[i], OUTPUT );
+    threshold[i]=EEPROMReadInt(i*2);
   };
   //calibrateSystem();
 }
@@ -88,6 +89,7 @@ void calibrateSystem() {
     delay(1000);
     Serial.println( analogRead(sensorPins[i])+5 );
     threshold[i] = analogRead(sensorPins[i])+5; //set threshold for sensor
+    EEPROMWriteInt(i*2,threshold[i]); //write new threshold to EEPROM
   }
   Serial.println( "pairing lasers and sensors" );
   for ( int i=0; i < numPins; i++ ) {
@@ -104,4 +106,21 @@ void calibrateSystem() {
       digitalWrite( laserPins[j], HIGH );
     }
   }
+}
+
+//This function will write a 2 byte integer to the eeprom at the specified address and address + 1
+void EEPROMWriteInt(int p_address, int p_value) {
+  byte lowByte = ((p_value >> 0) & 0xFF);
+  byte highByte = ((p_value >> 8) & 0xFF);
+
+  EEPROM.write(p_address, lowByte);
+  EEPROM.write(p_address + 1, highByte);
+}
+
+//This function will read a 2 byte integer from the eeprom at the specified address and address + 1
+unsigned int EEPROMReadInt(int p_address) {
+  byte lowByte = EEPROM.read(p_address);
+  byte highByte = EEPROM.read(p_address + 1);
+
+  return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
 }
